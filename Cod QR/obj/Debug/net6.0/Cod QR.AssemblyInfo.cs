14d1d8d1f27a2499a547168fc571e4e3b72f17be
vlaxcs <minciunescuvlad.business@gmail.com>
2025-01-25 22:14:25 +0200

    Commit

--- Cod QR/obj/Debug/net6.0/Cod QR.AssemblyInfo.cs
+++ Cod QR/obj/Debug/net6.0/Cod QR.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cod QR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a0668f99cd704c016a2b2bf49143d75e356bdcc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cod QR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cod QR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
