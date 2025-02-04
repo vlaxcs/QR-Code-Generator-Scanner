7bd78a51599d7244669abf93c00fe2101edc31f7
Dalv <futurevlad2@gmail.com>
2025-02-05 00:11:52 +0200

    Finished encoding message for a given message and message data type

---
+++ Cod QR/Encoding/MessageEncoder.cs
@@ -0,0 +1,17 @@
+﻿using QREncoder;
+
+public static class MessageEncoder {
+    readonly static Dictionary<DataType, IEncoder> encoders = new() {
+        { DataType.Numeric, new NumericEncoder() },
+        { DataType.Alphanumeric, new AlphanumericEncoder() },
+        { DataType.Byte, new ByteEncoder() }
+    };
+
+    public static QREncodedMessage EncodeMessage(DataType type, string message) {
+        if(type == DataType.Kanji) throw new Exception("Kanji not supported");
+
+        var bits = encoders[type].Encode(message);
+
+        return new QREncodedMessage(type, bits, message);
+    }
+}
--- Cod QR/Program.cs
+++ Cod QR/Program.cs
@@ -1,5 +1,11 @@
 ﻿using System.Text;
 
+var message = "314159265358979323846264s338327950288419716939";
+
+MessageEncoder.EncodeMessage(DataType.Numeric, message);
+
+return;
+
 var arr = Utility.ReadMatrixFromFile(@"../../../input.txt");
 
 //var code = new QRCode(arr);
