b4d759f4fdb39095e457a1260a44248f417a52bc
1BRG <balaceanurafael@gmail.com>
2025-02-05 22:27:54 +0200

    generator

---
+++ fac.cs
@@ -0,0 +1,13 @@
+﻿public struct QREncodedMessage
+{
+    public DataType type;
+    public byte[] bitsArray;
+    public string originalMessage;
+
+    public QREncodedMessage(DataType type, byte[] bitsArray, string originalMessage)
+    {
+        this.type = type;
+        this.bitsArray = bitsArray;
+        this.originalMessage = originalMessage;
+    }
+}
