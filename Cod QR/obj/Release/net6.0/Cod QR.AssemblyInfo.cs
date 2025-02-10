c34cb7252763bbc4767b7720aeea9f3bada8e6a4
vlaxcs <minciunecuvlad.business@gmail.com>
2025-02-10 09:03:13 +0200

    Second commit

--- Cod QR/obj/Release/net6.0/Cod QR.AssemblyInfo.cs
+++ Cod QR/obj/Release/net6.0/Cod QR.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cod QR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c8c9bc46767ee58a4305b8a447859879e003a37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cod QR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cod QR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
