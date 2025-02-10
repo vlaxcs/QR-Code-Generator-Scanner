e9b258be99cc8a8bdbae919621cc6bbe8c4abc01
vlaxcs <minciunecuvlad.business@gmail.com>
2025-02-10 19:48:25 +0200

    Finished frontend

--- docs/js/main.js
+++ docs/js/main.js
@@ -17,8 +17,6 @@ function pancaker(field){
 
 }
 window.onload = async function(){
-    // document.getElementById("get").onclick = handleGet;
-    // document.getElementById("post").onclick = handlePost;
     const preview = document.getElementById("preview");
     const result = document.getElementById("generated");
     if (preview){
@@ -36,22 +34,39 @@ window.onload = async function(){
     }
 
     const fileUploader = document.getElementById("scannerFile");
-    fileUploader.addEventListener("change", function(event) {
-        const file = event.target.files[0];
-        if(!file) {
-            preview.src = "none";
-            preview.visibility = "hidden";
-            return;
-        } else {
-            const reader = new FileReader();
-            reader.onload = function(e) {
-                console.log(e.target.result);
-                preview.src = e.target.result;
-            };
-    
-            reader.readAsDataURL(file);
-        }
-    });
+    if (fileUploader){
+        fileUploader.addEventListener("change", function(event) {
+            handleGet();
+            innerMessage = document.getElementsByClassName("message")[0];
+            innerMessage.style.color = "white";
+            innerMessage.innerHTML = "N/A";
+            const file = event.target.files[0];
+            if(!file) {
+                preview.src = "none";
+                preview.visibility = "hidden";
+                return;
+            } else {
+                const reader = new FileReader();
+                reader.onload = function(e) {
+                    console.log(e.target.result);
+                    preview.src = e.target.result;
+                };
+        
+                reader.readAsDataURL(file);
+            }
+        });
+    }
+
+    const scannerSend = document.getElementsByClassName("scanButton")[0];
+    if (scannerSend){
+        scannerSend.addEventListener("click", handlePost);
+    }
+
+    const generatorSend = document.getElementsByClassName("encodeButton")[0];
+    if (generatorSend){
+        generatorSend.addEventListener("click", handleGetGenerator);
+        generatorSend.addEventListener("click", handlePostGenerator);
+    }
 }
 
 var container;
@@ -59,7 +74,25 @@ var forecastTemplate;
 var infoElem;
 
 function handleGet() {
-    const promise = fetch("https://stable-well-moccasin.ngrok-free.app/helloworld", {
+    const promise = fetch("https://stable-well-moccasin.ngrok-free.app/scanner", {
+        headers: {
+            "ngrok-skip-browser-warning": "69420"
+        }
+    });
+    promise.then((response) => {
+        if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`);
+        }
+        return response.text();
+    }).then(function (text) {
+        handleReceive(text);
+    }).catch((error) => {
+        alert(error);
+    });
+}
+
+function handleGetGenerator() {
+    const promise = fetch("https://stable-well-moccasin.ngrok-free.app/generator", {
         headers: {
             "ngrok-skip-browser-warning": "69420"
         }
@@ -77,18 +110,14 @@ function handleGet() {
 }
 
 async function handlePost() {
-    var title = document.getElementById("messageTitle").value;
-    var content = document.getElementById("messageBody").value;
-
     var file = document.getElementById("scannerFile").files[0];
-
     if(!file) return;
 
     var formData = new FormData();
     formData.append("image", file);
 
     console.log(file.name, formData);
-    const response = await fetch("https://stable-well-moccasin.ngrok-free.app/helloworld", {
+    const response = await fetch("https://stable-well-moccasin.ngrok-free.app/scanner", {
         method: "POST",
         headers: {
             "ngrok-skip-browser-warning": "69420"
@@ -96,38 +125,70 @@ async function handlePost() {
         body: formData
     });
 
-
-    await console.log(response);
+    console.log(response);
     const result = await response.json();
 
-    document.getElementById("scanResult").innerText = result.message;
-
-    await console.log(result);
-
-    // fetch("https://stable-well-moccasin.ngrok-free.app/helloworld", {
-    //     method: "POST",
-    //     headers: {
-    //         "Content-Type": "application/json",
-    //         "ngrok-skip-browser-warning": "69420"
-    //     },
-    //     body: JSON.stringify({
-    //         "title":title,
-    //         "body":content
-    //     })
-    // }).then((response) => console.log(response));
+    innerMessage = document.getElementsByClassName("message")[0];
+    if (result.message[0] == "-" && result.message[1] == "1") {
+        innerMessage.style.color = "red";
+        innerMessage.innerHTML = "An erorr has occured";
+    } else {
+        const innerVersion = document.getElementsByClassName("version")[0];
+        innerVersion.innerHTML = result.version;
+        const innerECC = document.getElementsByClassName("ecc-level")[0];
+        innerECC.innerHTML = result.eccLevel;
+        innerMessage.style.color = "lightgreen";
+        innerMessage.innerHTML = result.message;
+    }
 }
 
 function handleReceive(data) {
-    infoElem.innerText = data;
-    // console.log(JSON.stringify(data));
-    // container.innerHTML = "";
+    uploaded = document.getElementsByClassName("uploaded")[0];
+    uploaded.innerHTML = data;
+}
 
-    // for(let i = 0; i < data.length; i++){
-    //     var card = forecastTemplate.content.cloneNode(true);
-    //     card.querySelector(".date").innerText = data[i].date;
-    //     card.querySelector(".temperatureC").innerText = data[i].temperatureC;
-    //     card.querySelector(".temperatureF").innerText = data[i].temperatureF;
-    //     card.querySelector(".summary").innerText = data[i].summary;
-    //     container.appendChild(card);
-    // }
+async function handlePostGenerator(){
+
+    userEccLevel = document.getElementById("errorCorrectionLevel").value;
+    userVersion = document.getElementById("version").value;
+    userMessage = document.getElementById("message").value;
+    if (!userEccLevel || !userVersion || !userMessage){
+        alert("Complete all fields!")
+    }
+
+    var aux;
+    switch (userEccLevel){
+        case "L": aux = 0; break;
+        case "M": aux = 1; break;
+        case "Q": aux = 2; break;
+        case "M": aux = 3; break;
+    }
+
+    const requestData = { 
+        message: userMessage,
+        minVersion: parseInt(userVersion),
+        eccLevel: aux,
+    };
+
+    console.log(JSON.stringify(requestData));
+
+    const response = await fetch("https://stable-well-moccasin.ngrok-free.app/generator", {
+        method: "POST",
+        headers: {
+            "ngrok-skip-browser-warning": "69420",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(requestData)
+    });
+
+    const blob = await response.blob();
+    const imageUrl = URL.createObjectURL(blob);
+    const qrCode = document.getElementById("generated");
+    qrCode.src = imageUrl;
+
+    const version = document.getElementsByClassName("version")[0];
+    version.innerHTML = response.headers.get("version");
+
+    const eccLevel = document.getElementsByClassName("ecc-level")[0];
+    eccLevel.innerHTML = response.headers.get("eccLevel") + " (" + userEccLevel + ")";
 }
